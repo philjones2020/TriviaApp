78bd242fa909eaf16aab17b90dc9f7729711a17f
DownToHuck <pjones2016@ufl.edu>
2020-07-15 20:41:44 -0400

    Remade project without --skip-active-record and added Question CRUD

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,33 @@
+class QuestionsController < ApplicationController
+	def show
+		@question = Question.find(params[:id])
+	end
+
+	def index
+		@questions = Question.all
+	end
+
+
+	def new
+	end
+
+	def create
+		@question = Question.new(question_params)
+
+		@question.save
+		redirect_to @question
+	end
+
+	def destroy
+		@question = Question.find(params[:id])
+		@question.destroy
+
+		redirect_to questions_path
+	end
+
+
+	private
+		def question_params
+			params.require(:question).permit(:question, :answer)
+		end
+end
