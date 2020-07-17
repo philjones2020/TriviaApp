1464e9fcd1caef6758c24ab5a538d7c59fe670b9
DownToHuck <pjones2016@ufl.edu>
2020-07-17 11:09:29 -0400

    Categorized the questions and added a way to view only category specifc questions

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,13 +1,17 @@
 class QuizController < ApplicationController
 	def sports
+		@questions = Question.where(category: "Sports")
 	end
 
 	def music
+		@questions = Question.where(category: "Music")
 	end
 
 	def entertainment
+		@questions = Question.where(category: "Entertainment")
 	end
 
 	def science
+		@questions = Question.where(category: "Science")
 	end
 end
